7047dc2525f95aa4a5a4a7c6000a786b70f6da6e
castrorena <renucastr19@gmail.com>
2025-06-05 01:37:05 -0300

    Add files via upload
    Hola profe perdon por la tardanza, lo hice con Oriana morleo y Agustin diaz

---
+++ tp-trivia/respuestas.php
@@ -0,0 +1,14 @@
+<?php
+$respuestas_correctas = [
+  "p1" => "Piel grasa",
+  "p2" => "Preparar la piel",
+  "p3" => "Brocha plana",
+  "p4" => "Resaltar zonas",
+  "p5" => "Spray fijador",
+  "p6" => "Primer",
+  "p7" => "Corrector",
+  "p8" => "Verde",
+  "p9" => "Brocha de difuminar",
+  "p10" => ["Base", "Corrector"]
+];
+?>
